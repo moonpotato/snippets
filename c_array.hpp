f0f39f38972808b50ae869391dd28b3457529bf4
moonpotato <git@funnelweb.xyz>
2018-03-16 00:45:25 +0800

    Make minor changes to comment formatting

--- c_array.hpp
+++ c_array.hpp
@@ -1,21 +1,21 @@
 #ifndef C_ARRAY_HPP_INCLUDED
 #define C_ARRAY_HPP_INCLUDED
 
-#include <cstddef> // For std::size_t
+#include <cstddef> // std::size_t
 
 // The following functions provide a safer way of getting information
 // about a C-style array at compile time. Unlike the `sizeof` method,
 // these will cause an error if unintentionally passed a pointer.
 
-// The number of elements in a C-style array
+// The number of elements in a C-style array.
 template <typename T, std::size_t N>
 constexpr std::size_t array_nelems (T(&)[N]) { return N; }
 
-// The size of each element in a C-style array
+// The size of each element in a C-style array.
 template <typename T, std::size_t N>
 constexpr std::size_t array_elemsz (T(&)[N]) { return sizeof (T); }
 
-// The total size occupied by a C-style array
+// The total size occupied by a C-style array.
 template <typename T, std::size_t N>
 constexpr std::size_t array_size   (T(&)[N]) { return sizeof (T[N]); }
 
