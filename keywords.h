2a8cc6114ee4d731895c0fb2bc72763dcdd997d1
moonpotato <git@funnelweb.xyz>
2018-03-17 15:34:32 +0800

    Add keywords.h and keywords.hpp
    Adds new "keywords" to C and/or C++. So far, these include:
    
    - "abstract" to mean "= 0" for pure virtual functions.

---
+++ keywords.h
@@ -0,0 +1,6 @@
+#ifndef KEYWORDS_H
+#define KEYWORDS_H
+
+// New "keywords" for C/C++
+
+#endif
---
+++ keywords.hpp
@@ -0,0 +1,14 @@
+#ifndef KEYWORDS_HPP
+#define KEYWORDS_HPP
+
+#include "keywords.h"
+
+// New "keywords" for C++
+
+#define ABSTRACT = 0
+
+#if defined(KW_USE_LOWERCASE)
+# define abstract ABSTRACT
+#endif
+
+#endif
